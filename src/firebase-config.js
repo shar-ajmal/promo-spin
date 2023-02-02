6756e8b3cac42f609627235740f83e311bc6526a
shar-ajmal <sherio777@gmail.com>
2023-02-01 22:54:20 -0800

    auth issue

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -90,7 +90,7 @@ export const signInWithGoogle = async () => {
   try {
     const res = await signInWithPopup(auth, googleProvider);
     const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const q = query(collection(db, "users"), where("user_id", "==", user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
       generateUser(user)
